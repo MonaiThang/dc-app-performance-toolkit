305a3f634d94ee461752508bf39950208aec366c
Jun Kim <jkim2@atlassian.com>
2023-09-19 10:33:27 +1000

    Add web panel click action

--- app/extension/jira/extension_locust.py
+++ app/extension/jira/extension_locust.py
@@ -35,6 +35,12 @@ def app_specific_action(locust):
     def app_user_interactions_web_panels():
         locust.get(f'/rest/app-usage/latest/user-interactions/web-panels?pluginKey={plugin_key}', catch_response=True)
 
+    @jira_measure("locust_app_user_interactions_web_panels_click_api")
+	def app_user_interactions_web_panels_click_api():
+		body = {"pluginKey": "com.atlassian.app.usage.app-usage-it-backdoor", "resourceLocation": "templates/web-panel/sample-web-panel.vm"}  # include parsed variables to POST request body
+		headers = {'content-type': 'application/json'}
+		locust.post('/rest/app-usage/latest/user-interactions/web-panels/clicks', body, headers, catch_response=True)
+
     @jira_measure("locust_app_custom_fields")
     def app_custom_fields():
         # custom fields - list of custom fields
@@ -55,7 +61,8 @@ def app_specific_action(locust):
     app_common_usage_jql_functions()
     app_common_usage_rest_api()
     app_user_interactions_page_views()
-    # app_user_interactions_web_panels()
+    app_user_interactions_web_panels()
+    app_user_interactions_web_panels_click_api()
     app_custom_fields()
     app_workflows()
     app_dashboards()
