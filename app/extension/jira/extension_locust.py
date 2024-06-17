1ac85af81da0f7dbe5488aefb98098a2e4814a72
Monai Thangsuphanich <mthangsuphanich@atlassian.com>
2024-06-17 12:51:39 +0700

    update test environment details

--- app/extension/jira/extension_locust.py
+++ app/extension/jira/extension_locust.py
@@ -5,7 +5,7 @@ logger = init_logger(app_type='jira')
 
 
 @jira_measure("locust_generate_lighthouse_events_action")
-@run_as_specific_user(username='admin', password='XlwYc4N_:kcDfTtzkxtSaC1a')  # run as specific user
+@run_as_specific_user(username='admin', password='admin')  # run as specific user
 def app_specific_action(locust):
     r = locust.get('/rest/lighthouse/1.0/alerts/create', catch_response=True)  # call app-specific GET endpoint
     content = r.content.decode('utf-8')   # decode response content
